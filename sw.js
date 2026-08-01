b4d6666481c8124eeaafbc1035c79b80d39445f9
kowal990804-jpg <kowal990804@gmail.com>
2026-08-01 22:47:05 +0200

    sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,8 @@
-const CACHE_NAME = 'tcl-cache-v1';
+// ZMIENIAJ TEN NUMER ZA KAŻDYM RAZEM GDY AKTUALIZUJESZ APLIKACJĘ (np. v2, v3, v4)
+const CACHE_NAME = 'tcl-cache-v2'; 
 
 self.addEventListener('install', (event) => {
+    self.skipWaiting(); // Wymusza natychmiastowe zainstalowanie nowej wersji
     event.waitUntil(
         caches.open(CACHE_NAME).then((cache) => {
             return cache.addAll([
@@ -9,6 +11,21 @@ self.addEventListener('install', (event) => {
                 './icon.png'
             ]);
         })
+    );
+});
+
+self.addEventListener('activate', (event) => {
+    // Usuwa stare wersje z pamięci telefonu
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (cacheName !== CACHE_NAME) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        }).then(() => self.clients.claim())
     );
 });
 
